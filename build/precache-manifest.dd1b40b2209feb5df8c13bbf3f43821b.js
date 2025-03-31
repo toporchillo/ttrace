8f85184e6cc4af3776903c265c75735c9d79d6f8
User <toporchillo@gmail.com>
2025-03-31 18:20:18 +0300

    Commit build

---
+++ build/precache-manifest.dd1b40b2209feb5df8c13bbf3f43821b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1443d7eec167a14558eed76e4cd1d3a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6e4a6be0181baddd6f",
+    "url": "/static/css/main.902ea603.chunk.css"
+  },
+  {
+    "revision": "99f8e1497213c7db58d2",
+    "url": "/static/js/2.13d72ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13d72ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6e4a6be0181baddd6f",
+    "url": "/static/js/main.6caa0d45.chunk.js"
+  },
+  {
+    "revision": "065b635e55d6cd389dcd",
+    "url": "/static/js/runtime-main.c0b9c4df.js"
+  },
+  {
+    "revision": "78e57cd0749390c1a9a258bf2eb27f7c",
+    "url": "/static/media/logo.78e57cd0.svg"
+  }
+]);
